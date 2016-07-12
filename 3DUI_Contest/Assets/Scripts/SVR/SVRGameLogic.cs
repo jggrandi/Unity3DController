07756e421474a4b996e135595b216894e3e0d00a
Jeronimo Grandi <jggrandi@gmail.com>
2016-07-12 09:45:58 -0300

    Fixed device distance orbiting the controlled object

--- 3DUI_Contest/Assets/Scripts/SVR/SVRGameLogic.cs
+++ 3DUI_Contest/Assets/Scripts/SVR/SVRGameLogic.cs
@@ -274,7 +274,7 @@ public class SVRGameLogic : MonoBehaviour {
 				Vector3 v = objControlledSmooth.transform.position;
 				v = v - (Vector3)r.GetColumn (2) * MainController.control.t.scaleMatrix.GetScale ().x;
 
-				c.deviceObject.transform.position = v - (Vector3)r.GetColumn (2);
+				c.deviceObject.transform.position = v ;
 				//c.deviceCameraCamera.transform.position = v - (Vector3)r.GetColumn (2);
 
 			}
